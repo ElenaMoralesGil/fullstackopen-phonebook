4a828291f5a3253540506d9d1258c3be0b8d83f0
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-08-14 12:55:42 +0100

    phonebook backend step 1 completed

---
+++ part3/index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let phonebook = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+app.get('/', (request, response) => {
+    response.send('<h1>Go to /api/phonebook to get tje list of phonebook entries</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
+
+const generateId = () => {
+    const maxId = phonebook.length > 0
+        ? Math.max(...phonebook.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const phone = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    phonebook = phonebook.concat(phone)
+
+    response.json(phone)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
