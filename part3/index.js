3a12668643807f257ec561d3b59b6a4a2190d8dd
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-08-20 17:22:37 +0100

    phonebook backend step 3 completed

--- part3/index.js
+++ part3/index.js
@@ -63,6 +63,16 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${phonebook.length} people</p><p>${new Date()}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = phonebook.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
